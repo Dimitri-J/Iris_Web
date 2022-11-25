9c2f0fa0b68e7b8eca5215028d1f975081180bb1
Dimitri-J <116066171+Dimitri-J@users.noreply.github.com>
2022-11-25 10:58:50 +0100

    Update StreamLit_IRIS.py

--- StreamLit_IRIS.py
+++ StreamLit_IRIS.py
@@ -134,6 +134,18 @@ with col4:
 
 
 if st.button("Calcul de l'espèce d'iris"):
+    import time
+
+    html_string = """
+                <audio autoplay>
+                <source src="https://www.orangefreesounds.com/wp-content/uploads/2022/04/Small-bell-ringing-short-sound-effect.mp3" type="audio/mp3">
+                </audio>
+                """
+
+    sound = st.empty()
+    sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
+    time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
+    sound.empty()  # optionally delete the element afterwards
     col_b1, col_b2 = st.columns([2,1])
     test_iris.append([l_sepal, w_sepal, l_petal, w_petal])
     print(test_iris)
